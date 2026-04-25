2893f98ba5fb809c7de2ec54ba2a6f4484575500
Miguel <shiyatsoku@gmail.com>
2026-04-24 22:26:52 -0300

    commit 3

--- Usuario.java
+++ Usuario.java
@@ -2,32 +2,28 @@ import java.util.ArrayList;
 
 public class Usuario {
 
-    private String nome;
-    private ArrayList<Playlist> playlists = new ArrayList<>();
+    protected String nome;
+    protected String email;
+    protected ArrayList<Playlist> playlists = new ArrayList<>();
 
-    public Usuario() {}
-
-    public Usuario(String nome) {
-        setNome(nome);
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        if (nome == null || nome.trim().isEmpty()) {
-            throw new IllegalArgumentException("Nome inválido");
-        }
-        this.nome = nome.trim();
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
     }
 
     public ArrayList<Playlist> getPlaylists() {
         return playlists;
     }
 
-    // adicionar a playlist na lista de playlists do usuário
     public void criarPlaylist(String nome) {
         playlists.add(new Playlist(nome));
+    }
+
+    public void reproduzirMusica(Musica m) {
+        System.out.println("Reproduzindo: " + m.getTitulo());
+    }
+
+    public void exibirMenu() {
+        System.out.println("Menu padrão");
     }
 }
